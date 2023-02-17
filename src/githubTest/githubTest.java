b7aceede006f39c34bfd12098975deb8ff1138e8
fred8260 <frederik.eriksen@live.dk>
2023-02-17 11:09:31 +0100

    Lavede en ny sout test

--- src/githubTest/githubTest.java
+++ src/githubTest/githubTest.java
@@ -4,5 +4,7 @@ public class githubTest {
     public static void main(String[] args) {
         System.out.println("Hej");
         System.out.println("igen");
+
+        System.out.println("Pull test :-)");
     }
 }
